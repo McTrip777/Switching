a89424ec94e83e9cd59bcb6594b405cb9a53d388
Jacob <jacob.m.mcfaul@gmail.com>
2020-07-22 13:09:15 -0700

    switch component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule,
